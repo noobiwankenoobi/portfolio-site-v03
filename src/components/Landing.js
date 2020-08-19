2f5067403e7c8d043dbb18c51c4f29aa0e7c58a2
Ross Hazelett <rhhazelett@gmail.com>
2020-08-18 23:01:02 -0400

    landing page done

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,4 +1,6 @@
 import React from "react";
+// ICONS
+import { BsArrowDownShort } from "react-icons/bs";
 
 const Landing = () => {
   //////////////////
@@ -7,13 +9,18 @@ const Landing = () => {
   const LandingJSX = (
     <div className="landing-section bg-gray-900 flex flex-col justify-center items-center md:h-screen md:w-screen text-gray-100 ">
       <div className="flex flex-col justify-center items-center mx-auto my-auto">
-        <h1 className="text-5xl tracking-widest">ROSS HAZELETT</h1>
-        <h1 className="text-3xl tracking-widest">web developer</h1>
-        <button className="text-lg md:mt-6 tracking-wide border border-gray-600 p-2 rounded">
+        <h1 className="text-5xl tracking-widest">
+          <span className="text-cyberdesatturq">ROSS</span> HAZELETT
+        </h1>
+        <h1 className="text-3xl tracking-widest">
+          web <span className="text-cyberburnorange">developer</span>
+        </h1>
+        <button className="text-lg text-gray-300 tracking-wider md:mt-6 border border-gray-600 bg-opacity-75 p-2 rounded hover:shadow-lg transition duration-500 ease-in-out transform hover:border-2 hover:scale-105 hover:border-cyberlightblue">
           VIEW PROJECTS
         </button>
-        <h1 className="text-xl md:mt-4">
-          or keep scrolling <i className="fa fa-angle-down down-arrow"></i>
+        <h1 className="flex tracking-wider justify-center items-center text-xl md:mt-6 ">
+          or keep scrolling{" "}
+          <BsArrowDownShort className="animate-bounce text-cyberdesatturq" />
         </h1>
       </div>
     </div>
